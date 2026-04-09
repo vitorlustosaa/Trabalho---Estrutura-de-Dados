56aa654ce7c1e4b97946a6cfc883e1baaf067e4a
vitorlustosaa <vitorlustosapinheiro@gmail.com>
2026-04-09 17:07:22 -0300

    Create tribunal.c with initial comments and includes
    Add initial structure and comments for tribunal.c

---
+++ tribunal.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "tribunal.h"
+
+// [OBSERVAÇÃO PARA O GRUPO - TAREFAS QUE AINDA TÊM QUE SER FEITAS]:
+// 1. ITEM 1 (CONCATENAR): Implementar leitura de arquivos e gerar o arquivo unificado "csv"
+// 2. ITEM 2 (RESUMO): Criar a lógica para calcular as Metas 1, 2A, 2Ant, 4A e 4B usando as fórmulas do PDF
+// 3. ITEM 3 (BUSCA): Implementar a busca na lista pelo município informado e salvar em um novo arquivo .csv
+// 4. FUNÇÃO AUXILIAR: Precisamos criar a função para INSERIR na lista (TAD Lista) tratada em sala
+// 5. BIBLIOTECA: Usem 'string.h' para comparar os nomes dos tribunais e municípios
