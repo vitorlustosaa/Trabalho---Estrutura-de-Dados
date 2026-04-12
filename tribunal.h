82b75fb9015665c273bf2beca44dfc690d1028b9
vitorlustosaa <vitorlustosapinheiro@gmail.com>
2026-04-12 18:17:00 -0300

    Update tribunal.h

--- tribunal.h
+++ tribunal.h
@@ -1,5 +1,3 @@
-
-
 #ifndef TRIBUNAL_H
 #define TRIBUNAL_H
 
@@ -9,22 +7,23 @@ typedef struct DadoTribunal {
     char ramo_justica[100];
     char sigla_grau[20];
     char uf_oj[10];
-    char municipio_oj[150]; 
+    char municipio_oj[150];
     int id_ultimo_oj;
     char nome[250];
     char mesano_cnm1[20];
     char mesano_sent[20];
-    int casos_novos_2026;
-    int julgados_2026;
-    int prim_sent2026;
-    int suspensos_2026;
-    int dessobrestados_2026;
+    int casos_novos_2026, julgados_2026, prim_sent2026, suspensos_2026, dessobrestados_2026;
+    float cumprimento_meta1; // Coluna 16
     
-    // Campos das Metas (Iniciando em 0)
+    // Campos para Metas 2 e 4 (Colunas 17 a 33)
     int distm2_a, julgm2_a, suspm2_a;
+    float cumprimento_meta2a;
     int distm2_ant, julgm2_ant, suspm2_ant, desom2_ant;
+    float cumprimento_meta2ant;
     int distm4_a, julgm4_a, suspm4_a;
+    float cumprimento_meta4a;
     int distm4_b, julgm4_b, suspm4_b;
+    float cumprimento_meta4b;
 
     struct DadoTribunal *proximo;
 } DadoTribunal;
